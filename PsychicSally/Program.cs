ce4307f71b0ddb7ce79dcd9e72e51343b584c4e1
andrujoh <andru_joh@yahoo.no>
2017-07-02 19:27:44 +0200

    Add basic program

--- PsychicSally/Program.cs
+++ PsychicSally/Program.cs
@@ -10,6 +10,36 @@ namespace PsychicSally
   {
     static void Main(string[] args)
     {
+      var maxTries = 10;
+      var random = new Random();
+      var numberToGuess = random.Next(0, 100);
+      var numberNotGuessed = true;
+
+      for (var tries = 0; numberNotGuessed && tries < maxTries; tries++)
+      {
+        Console.Write("Skriv inn et tall mellom 1 og 100: ");
+        var input = Console.ReadLine();
+        int guessedNumber;
+        if (int.TryParse(input, out guessedNumber))
+        {
+          if (guessedNumber > numberToGuess)
+          {
+            Console.WriteLine("Feil, du gjettet for høyt");
+          }
+          else if (guessedNumber < numberToGuess)
+          {
+            Console.WriteLine("Feil, du gjettet for lavt");
+          }
+          else
+          {
+            Console.WriteLine("Gratulerer, du gjettet riktig!");
+            break;
+          }
+
+        }
+
+        Console.WriteLine("Trykk en tast for å avslutte");
+      }
     }
   }
 }
