77ad536ca778443d6626aa0208ce660742f54463
andrujoh <andru_joh@yahoo.no>
2017-07-02 20:45:16 +0200

    Add maxTries change from console and refactor input method

--- PsychicSally/Program.cs
+++ PsychicSally/Program.cs
@@ -10,36 +10,63 @@ namespace PsychicSally
   {
     static void Main(string[] args)
     {
+      // Change maxTries to how many tries you want
       var maxTries = 10;
+      foreach (var argument in args)
+      {
+        var argumentParts = argument.Split('=');
+        if (argument.ToLower().StartsWith("-maxTries"))
+        {
+          var argumentName = argumentParts[0];
+          var argumentValue = argumentParts[1];
+          maxTries = int.Parse(argumentValue);
+        }
+      }
       var random = new Random();
       var numberToGuess = random.Next(0, 100);
       var numberNotGuessed = true;
 
       for (var tries = 0; numberNotGuessed && tries < maxTries; tries++)
       {
-        Console.Write("Skriv inn et tall mellom 1 og 100: ");
-        var input = Console.ReadLine();
-        int guessedNumber;
-        if (int.TryParse(input, out guessedNumber))
-        {
-          if (guessedNumber > numberToGuess)
-          {
-            Console.WriteLine("Feil, du gjettet for høyt");
-          }
-          else if (guessedNumber < numberToGuess)
-          {
-            Console.WriteLine("Feil, du gjettet for lavt");
-          }
-          else
-          {
-            Console.WriteLine("Gratulerer, du gjettet riktig!");
-            break;
-          }
+        int guessedNumber = ReadNumber();
 
+        if (guessedNumber > numberToGuess)
+        {
+          Console.WriteLine("Feil, du gjettet for høyt");
+        }
+        else if (guessedNumber < numberToGuess)
+        {
+          Console.WriteLine("Feil, du gjettet for lavt");
+        }
+        else
+        {
+          Console.WriteLine("Gratulerer, du gjettet riktig!");
+          break;
         }
 
-        Console.WriteLine("Trykk en tast for å avslutte");
+
       }
+
+      Console.WriteLine("Trykk en tast for å avslutte");
+
+    }
+
+    private static int ReadNumber()
+    {
+      Console.Write("Skriv inn et tall mellom 1 og 100: ");
+      var input = Console.ReadLine();
+      int guessedNumber;
+
+      if (int.TryParse(input, out guessedNumber))
+      {
+        return guessedNumber;
+      }
+      else
+      {
+        Console.WriteLine("Du må skrive inn et gyldig heltall");
+        return ReadNumber();
+      }
+
     }
   }
 }
